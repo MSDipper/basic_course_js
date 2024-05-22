df4a79d1cf3ed6b29f0633a082d495d0d6cc5ca6
msd <github5248@gmail.com>
2024-05-22 21:19:08 +0300

    исправил round на функцию sqrt

--- 4-navigator/app.js
+++ 4-navigator/app.js
@@ -3,4 +3,4 @@ const addressLong = 10;
 const positionLat = 5;
 const positionLong = 10;
 
-console.log(Math.round(((addressLong - addressLat) ** 2 + (positionLong - positionLat) ** 2) ** 0.5));
+console.log(Math.sqrt(((addressLong - addressLat) ** 2 + (positionLong - positionLat) ** 2) ** 0.5));
