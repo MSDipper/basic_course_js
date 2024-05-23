3031d50842bf346ee8ab3022c5b522d28ba4d78f
msd <github5248@gmail.com>
2024-05-24 00:59:10 +0300

    Update app.js

--- 4-navigator/app.js
+++ 4-navigator/app.js
@@ -1,6 +1,6 @@
 const addressLat = 5;
 const addressLong = 10;
-const positionLat = 5;
+const positionLat = 15;
 const positionLong = 10;
 
-console.log(Math.round(Math.sqrt(((addressLong - addressLat) ** 2 + (positionLong - positionLat) ** 2) ** 0.5)));
+console.log(Math.round(Math.sqrt(((positionLat - addressLat) ** 2 + (positionLong - addressLong) ** 2) ** 0.5)));
